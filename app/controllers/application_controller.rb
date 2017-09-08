72fa9ba070bca196d3f16c792f35d55bc3c8ba78
danale <danale@danales-MacBook-Pro.local>
2017-09-08 14:03:56 -0400

    Added installation requirements for petergate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
+  include SetSource
+  include CurrentUserConcern
+
+  before_action :set_title
+
+  def set_title
+    @page_title = "Woodbury Farmers Market"
+  end
 end
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,11 +5,13 @@ class CategoriesController < ApplicationController
   # GET /categories.json
   def index
     @categories = Category.all
+    @page_title = "Woodbury Farmers Market"
   end
 
   # GET /categories/1
   # GET /categories/1.json
   def show
+    @page_title = @category.title
   end
 
   # GET /categories/new
