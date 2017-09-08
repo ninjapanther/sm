5d682d1f2e64fca7bed7ca7e1d0a714775dde5ad
Joshua Waiswa <jwaiswa7@gmail.com>
2017-09-08 08:01:40 -0400

    Added bootstrap theme for User interface landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
-  resources :homes
   resources :categories
   resources :products
-  root "homes#index"
+  root "landings#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
