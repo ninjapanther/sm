5bb268c3b958cc678c5a6a1f7955312ea9aa6c15
Joshua Waiswa <jwaiswa7@gmail.com>
2017-09-09 02:22:29 -0400

    Bootstrap added to devise pages

--- db/schema.rb
+++ db/schema.rb
@@ -18,11 +18,6 @@ ActiveRecord::Schema.define(version: 20170821194009) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "homes", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "products", force: :cascade do |t|
     t.string   "summary"
     t.text     "description"
