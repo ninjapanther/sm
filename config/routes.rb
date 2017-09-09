5600ac629e588846faa26b705b53b015223295d4
danale <danale@danales-MacBook-Pro.local>
2017-09-09 15:46:36 -0400

    Updated styles for master layout file

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :homes
   resources :categories
   resources :products
-  root "homes#index"
+
+  root to: "homes#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
