1f219b5998cb397cb26988dc6ab34c80288c83ec
Joshua Waiswa <jwaiswa7@gmail.com>
2017-09-12 10:30:53 -0400

    Cart subtotal for items automatically calculated

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,11 @@
 class Cart < ApplicationRecord
   belongs_to :user
   belongs_to :product
+
+  before_save :calculate_subtotal
+
+  private
+  def calculate_subtotal
+    self.subtotal = self.quantity*self.product.price
+  end
 end
