3e73f513eff7ac051d7dcb33f96cdf667c75b291
Joshua Waiswa <jwaiswa7@gmail.com>
2017-09-12 10:17:42 -0400

    Cart model added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170912120350) do
+ActiveRecord::Schema.define(version: 20170912141202) do
 
   create_table "carts", force: :cascade do |t|
     t.integer  "user_id"
