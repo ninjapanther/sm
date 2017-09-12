44d3cc61870f4808abbd5d28c45e69d6ae5f0c8e
Joshua Waiswa <jwaiswa7@gmail.com>
2017-09-12 09:34:18 -0400

    Added price to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170912114119) do
+ActiveRecord::Schema.define(version: 20170912120350) do
+
+  create_table "carts", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "product_id"
+    t.integer  "quantity"
+    t.float    "subtotal"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_carts_on_product_id"
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -31,6 +42,7 @@ ActiveRecord::Schema.define(version: 20170912114119) do
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
     t.boolean  "active"
+    t.float    "price"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
