0faddcb7fd0686634ebf61b77dd5c25f7cbe07a2
blog <rubygig@yahoo.com>
2017-09-12 15:36:39 +0530

    database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170911083631) do
+ActiveRecord::Schema.define(version: 20170912095852) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -18,9 +18,9 @@ ActiveRecord::Schema.define(version: 20170911083631) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "homes", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "categories_products", id: false, force: :cascade do |t|
+    t.integer "category_id", null: false
+    t.integer "product_id",  null: false
   end
 
   create_table "products", force: :cascade do |t|
