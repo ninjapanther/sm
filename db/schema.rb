dcac35aa11b59a4477d885694064da734605bda6
Joshua Waiswa <jwaiswa7@gmail.com>
2017-09-12 07:42:19 -0400

    Added Active and user to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170911083631) do
+ActiveRecord::Schema.define(version: 20170912114119) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20170911083631) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
+    t.boolean  "active"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
