1ae2f73aa4787fa0e2292fce1934745ca55c9c4f
blog <rubygig@yahoo.com>
2017-09-13 22:17:41 +0530

    l

--- db/schema.rb
+++ db/schema.rb
@@ -23,26 +23,20 @@ ActiveRecord::Schema.define(version: 20170912141202) do
     t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
-
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "categories_products", id: false, force: :cascade do |t|
-    t.integer "category_id", null: false
-    t.integer "product_id",  null: false
-  end
-
   create_table "products", force: :cascade do |t|
-    t.string   "name"
+    t.string   "summary"
     t.text     "description"
     t.string   "image"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "user_id"
-    t.boolean  "active", default: false
+    t.boolean  "active"
     t.float    "price"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
