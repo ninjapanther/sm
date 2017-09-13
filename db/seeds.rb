e177b720fd943f98c15bf47cfeb847387166ab6c
Joshua Waiswa <jwaiswa7@gmail.com>
2017-09-13 05:45:29 -0400

    Minor changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+  User.create!(name:"TestUser",password:"password",password_confirmation:"password",email:"test@example.com")
+  Product.create([{name:"Product",description:"test product",price:120,user:User.last},{name:"Product",description:"test product",price:120,user:User.last},{name:"Product",description:"test product",price:120,user:User.last}])
