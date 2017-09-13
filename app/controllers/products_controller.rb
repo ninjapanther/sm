c3c69def92ab14724206af4a73c6b2e2d59cc1c6
danale <danale@danales-MacBook-Pro.local>
2017-09-13 08:35:14 -0400

    Implemented partials for products page, font awesome and pagination

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,7 +6,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    @products = Product.page(params[:page]).per(5)
     @page_title = "Products"
   end
 
