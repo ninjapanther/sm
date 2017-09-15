dde84c78c231aa81787af6b23c8f27a1ea800885
danale <danale@danales-MacBook-Pro.local>
2017-09-14 21:15:01 -0400

    Implemented refactor for navigation and active statuses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,4 +19,35 @@ module ApplicationHelper
   def copyright_generator
      MicrourbViewTool::Renderer.copyright 'Oceancode', 'All rights reserved'
   end
+
+  def nav_items
+    [
+      {
+        url: root_path,
+        title: 'Home'
+      },
+       {
+        url: products_path,
+        title: 'Products'
+      },
+       {
+        url: categories_path,
+        title: 'Categories'
+      }
+    ]
+  end
+
+  def nav_helper style, tag_type
+    nav_links = ''
+
+    nav_items.each do |item|
+      nav_links << "<#{tag_type}><a href='#{item[:url]}' class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+    end
+
+    nav_links.html_safe
+  end
+
+  def active? path 
+    "active" if current_page? path
+  end
 end
